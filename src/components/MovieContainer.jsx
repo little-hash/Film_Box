f4ba20a26d68e3212175d64ca10067a10d9111a7
Ezra <ezraraddai166@gmail.com>
2025-01-30 06:23:36 +0300

    firstcommit

---
+++ src/components/MovieContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieContainer({ movies }) {
+  return (
+    <div className="movie-container">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+}
+
+export default MovieContainer;
